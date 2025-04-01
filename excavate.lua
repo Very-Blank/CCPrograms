cdd8abff440be7fd0f90d2a152dbefb409c4828c
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-04-01 18:53:36 +0300

    Changes

--- excavate.lua
+++ excavate.lua
@@ -7,20 +7,25 @@ if #args ~= 3 then
 	return
 else
 	for _, arg in ipairs(args) do
-		if type(arg) ~= "number" then
+		local num = tonumber(arg)
+		if num == nil then
 			printError("Argument wasn't a number")
 			return
 		end
 
-		if math.floor(arg) == arg then
+		if math.floor(num) == num then
 			printError("Argument wasn't a whole number")
 			return
 		end
 	end
 end
 
+-- local x = tonumber(args[1])
+-- local y = tonumber(args[2])
+local z = tonumber(args[3])
+
 local function destroyForward()
-	for _ = 1, args[3], 1 do
+	for _ = 1, z, 1 do
 		turtle.dig()
 	end
 end
